1c9ea4e3a74c17c506c027cc32e17b12488e35af
Alexander Friberg <alexanderfriberg1999@gmail.com>
2025-08-22 04:25:10 +0200

    Removed old logic from storage

--- modules/storage.py
+++ modules/storage.py
@@ -1,6 +1,5 @@
 import os
 import json
-from classlib import Item
 from .config import DB_PATH, SHELF_PATH
 
 
@@ -12,17 +11,6 @@ def setup():
         with open(SHELF_PATH, "w") as f:
             json.dump({}, f)
 
-
-def add_item_json(item):
-    try:
-        db = get_db()
-        db.update(item)
-        with open(DB_PATH, 'w') as f:
-            json.dump(db, f, indent=4)
-    except Exception as e:
-        raise e(f"{e}")
-
-
 def get_db():
     with open(DB_PATH, 'r') as f:
         return json.load(f)
@@ -30,28 +18,6 @@ def get_db():
 def get_shelf():
     with open(SHELF_PATH, 'r') as f:
         return json.load(f)
-
-
-def delete_item(id):
-    try:
-        data = get_db()
-        data = [item for item in data if item['id'] != id]
-        with open(DB_PATH, 'w') as f:
-            json.dump(data, f, indent=4)
-        return f"item with id: '{id}' was succesfully removed"
-    except:
-        raise Exception("Item was not found")
-
-
-def update_item(id, item_update):
-    try:
-            data = get_db()
-            data[id] = Item.to_dict(item_update)        
-            with open(DB_PATH, 'w') as f:
-                json.dump(data, f, indent=4)
-            return f"Item with ID '{id}' was successfully updated."
-    except Exception as e:
-        raise e("Item was not found")
 
 def save_db(data):
     with open(DB_PATH, 'w') as f:
